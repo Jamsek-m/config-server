8c47b9ae3bb905b01e97e53f6b09b6cbc053089b
Miha Jamsek <miha_jamsek@windowslive.com>
2019-04-22 00:57:48 +0200

    added pages and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
+import {Error404PageComponent} from "./pages/404-page/error404-page.component";
+import {IndexPageComponent} from "./pages/index-page/index-page.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: "", pathMatch: "full", component: IndexPageComponent},
+    {path: "login", component: LoginPageComponent},
+    {path: "404", component: Error404PageComponent},
+    {path: "**", redirectTo: "/404"}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import {NgModule} from "@angular/core";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
+import { IndexPageComponent } from './pages/index-page/index-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { Error404PageComponent } from './pages/404-page/error404-page.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        IndexPageComponent,
+        LoginPageComponent,
+        Error404PageComponent
     ],
     imports: [
         BrowserModule,
